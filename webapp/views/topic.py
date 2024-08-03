7adba88046394476c8ed648bb1187a55a0b16fa9
azamat <azamatclb@gmail.com>
2024-08-03 17:38:30 +0600

    Добавил пагинацию

--- webapp/views/topic.py
+++ webapp/views/topic.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView
 
@@ -11,6 +12,7 @@ class TopicListView(ListView):
     template_name = 'topic_templates/topics.html'
     model = Topic
     context_object_name = 'topics'
+    paginate_by = 4
 
 
 class TopicCreateView(CreateView, LoginRequiredMixin):
@@ -28,10 +30,15 @@ class TopicDetailView(DetailView, LoginRequiredMixin):
     template_name = 'topic_templates/topic_detail.html'
     model = Topic
     context_object_name = 'topic'
+    paginate_by = 2
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         topic = self.get_object()
-        context['comments'] = topic.comments.all()
+        comments = topic.comments.all()
+        paginator = Paginator(comments, 2)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['comments'] = page_obj
         context['comment_form'] = CommentForm()
         return context
