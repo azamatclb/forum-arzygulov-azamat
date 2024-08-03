a04b3ab3c7f7f32e0ddb9133970c09a0d82d37e6
azamat <azamatclb@gmail.com>
2024-08-03 11:38:14 +0600

    Добавил приложение account

--- forum/settings.py
+++ forum/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "webapp.apps.WebappConfig"
+    "webapp",
+    "account"
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'forum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'webapp/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
